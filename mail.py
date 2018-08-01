9cb995ddcd08852b4a6f414277b3570f32c0674e
Ricky Hoberecht <Richard.Hoberecht@cypress.com>
2018-08-01 07:52:26 -0700

    Refactor, and use EmailMessage instead of Mimetype object stuff

--- mail.py
+++ mail.py
@@ -5,22 +5,13 @@ import smtplib
 
 # Determine which type the attachment is
 import mimetypes
-import email.mime.multipart as multipart
-
-# Message attachments
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email.mime.audio import MIMEAudio
-from email.mime.image import MIMEImage
-
-# Encoder for unknown attachment type
-from email import encoders
+from email.message import EmailMessage
 
 class Dev:
     '''SMTP Mail Sender!
-    
+
     Send a quick message / attachment.
-    
+
     Example:
         >>> dev = Dev(user=someone@mail.com, password='', host='', port=123)
         >>> dev.open()
@@ -35,91 +26,64 @@ class Dev:
             host (str): Host server's SMTP address
             port (int): Host server's SMTP Port
         '''
+        super(Dev, self).__init__()
+
         # Server / User information
         self.user = user
         self.password = password
         self.host = host
         self.port = port
-        
+
         # Server connection
         self.server = None
-    
+
     def open(self):
         '''Open up the connection to the server, and log in.'''
         self.server = smtplib.SMTP(host=self.host, port=self.port)
-        
+
         # Start the secure connection
         self.server.starttls()
-        
         self.server.login(self.user, self.password)
-    
+
     def close(self):
         '''Close down the connection to the server'''
         if self.server:
             self.server.quit()
             del self.server
-            
+
     def send(self, text, *args, to=None, subject=''):
         '''Send text, as well as attachments.
-        
+
         Args:
             text (str): Message text.
-            *args: Attachment files. The MIME type will be guessed, based on 
+            *args: Attachment files. The MIME type will be guessed, based on
                 the file extension.
             to (str): Address to send the message to. Default is user.
             subject (str): Subject of the message. Default is ''.
         '''
         if not to:
             to = self.user
-        
-        message = multipart.MIMEMultipart()
+
+        message = EmailMessage()
         message['From'] = self.user
         message['To'] = to
         message['Subject'] = subject
-        
+
+        # Add message content
+        message.set_content(text)
+
         # Parse attachments
         for attachment in args:
-            message.attach(self._get_attachment(attachment))
-        
-        # Attach the text
-        message.attach(MIMEText(text, 'plain'))
-        
+            # Get the MIME Type
+            ctype, encoding = mimetypes.guess_type(attachment)
+            if ctype is None or encoding is not None:
+                ctype = 'application/octet-stream'
+
+            main_type, subtype = ctype.split('/', 1)
+            file_name = os.path.basename(attachment)
+
+            with open(attachment, 'rb') as input_file:
+                message.add_attachment(input_file.read(), maintype=main_type, subtype=subtype, filename=file_name)
+
         # Send the message!
         self.server.send_message(message)
-        
-    def _get_attachment(self, file_path):
-        '''Form the attachment.
-        
-        Args:
-            file_path (str): Path to the attachment.
-        
-        Returns:
-            The Attachment.
-        '''
-        # Get the MIME Type
-        ctype, encoding = mimetypes.guess_type(file_path)
-        if ctype is None or encoding is not None:
-            ctype = 'application/octet-stream'
-        
-        main_type, subtype = ctype.split('/', 1)
-        
-        # Form the attachment
-        with open(file_path, 'r') as input_file:
-            if main_type == 'text':
-                attachment = MIMEText(input_file.read(), _subtype=subtype)
-            elif main_type == 'image':
-                attachment = MIMEImage(input_file.read(), _subtype=subtype)
-            elif main_type == 'audio':
-                attachment = MIMEAudio(input_file.read(), _subtype=subtype)
-            else:
-                attachment = MIMEBase(main_type, subtype)
-                attachment.set_payload(input_file.read())
-                encoders.encode_base64(attachment)
-        
-        # Get the file name
-        file_name = os.path.basename(file_path)
-        
-        # Set the Header
-        attachment.add_header('Content-Disposition', 'attachment', filename=file_name)
-        
-        return attachment
