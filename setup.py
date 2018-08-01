a7d0468f65ca57e66390b71cf34c77bef733a082
Ricky Hoberecht <Richard.Hoberecht@cypress.com>
2018-08-01 13:09:22 -0700

    Add initial version of the setup.py, which is the installer for the thing

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+# http://setuptools.readthedocs.io/en/latest/setuptools.html
+# Ex: https://bitbucket.org/pyglet/pyglet/src/f48574e6c61ce5f6780608e75b72e9dd8733ec7e/setup.py?fileviewer=file-view-default
+# Ex: https://github.com/django/django/blob/master/setup.py
+
+from setuptools import setup
+
+setup(name='mail',
+    version='1.0.0',
+    description='Send an Email using a Friendly Interface',
+    author='Richard Hoberecht (RIHO)',
+    author_email='Richardhob@gmail.com',
+    url='http://git-ore.aus.cypress.com/riho/pbats',
+    packages=['mail'],
+    # include_package_data=True,
+    license='Use at your own risk!',
+)
